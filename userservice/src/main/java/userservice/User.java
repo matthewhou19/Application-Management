b68e7e823a49a760df589bf414f72bef02f2bbb3
matthewhou19 <80615048+matthewhou19@users.noreply.github.com>
2023-04-28 13:22:59 -0700

    mysql local setup

---
+++ userservice/src/main/java/userservice/User.java
@@ -0,0 +1,27 @@
+package userservice;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+
+    private String lastname;
+    private String email;
+    private String password;
+
+}
